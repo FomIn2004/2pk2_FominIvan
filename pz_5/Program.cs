a91729449337f0e214f3bb669b799e282fe876d9
fominis2004@gmail.com <fominis2004@gmail.com>
2021-09-28 10:06:42 +0500

    test

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,16 +6,64 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
+            
             int k;
-            for (k = 30; k <= 150; k += 15 )
+           
+            Console.WriteLine("Задание 1");
+            for (k = 30; k <= 150; k += 15 ) // пока к меньше либо равно 150 - работает, шаг - к+= 15 - через каждые 15 чисел
             {
 
-                Console.WriteLine(k);
+               Console.WriteLine(k); // вывод
             }
-            Console.ReadLine();
+            //Console.ReadLine();
 
 
             // Задание 2
+            
+            Console.WriteLine("Задание 2");
+            char sim = 'н'; // объявление
+            for (var i = 0; i < 5; i++) // пока i меньше 5, выполняется
+            {
+                Console.Write(" " + sim); // вывод
+                sim++;
+
+           }
+            // Задание 3
+            Console.WriteLine(" ");
+            
+            Console.WriteLine("Задание 3");
+
+            var a = "#"; // объявление переменной с символом 
+            for (int i = 0; i != 6 ; i++) // пока i не будет 6 - работает
+            {
+                for (int j = 0; j != 10; j++) // влож. цикл для другой стороны прямоугольника
+                {
+                    Console.Write(a);// вывод
+
+
+                }
+                Console.WriteLine();
+            }
+
+            // Задание 4
+            for (int i = 0; i != 100; i++) // цикл до 100
+            {
+                var number = i % 18; // если кратно 18 
+                if(number == 0) 
+                {
+                    Console.WriteLine(i); // вывод
+                }
+
+            }
+            // Задание 5
+
+
+
+
+
+            
+
         }
     }
 }
+
