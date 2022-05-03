77265bdd6cd6d7e5bfcc54c4c2ad9141e3c72296
FomIn2004 <91515536+FomIn2004@users.noreply.github.com>
2022-05-03 16:06:37 +0500

    Add files via upload

---
+++ 3.05.2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace pz
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            double[] dob = new double[3];
+            char[] qq = { 'H', 'E', 'L' };
+            dob[0] = 134523;
+            dob[1] = 33454567;
+            dob[2] = 4745664;
+            char* qwert;
+            for (int d = 0; d < dob.Length; d++)
+            {
+                for (int i = 0; i < sizeof(double); i++)
+                {
+                    fixed (double* gg = &dob[d])
+                    {
+                        qwert = (char*)gg;
+                        qwert[i] = qq[d];
+                        Console.WriteLine(*qwert);
+                        Console.ReadKey();
+
+                    }
+                }
+
+            }
+        }
+    }
+}
